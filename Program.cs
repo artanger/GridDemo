99851f4f0ef2505ba1d3e26c49a918405a63ddb4
artanger <artimag666@gmail.com>
2020-02-12 17:12:21 +0200

    Fill the grid

--- Program.cs
+++ Program.cs
@@ -10,8 +10,27 @@ namespace GridDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int[,] grid = new int[3, 3];
+            grid[0, 0] = 1;
+            grid[0, 1] = 2;
+            grid[0, 2] = 3;
+            grid[1, 0] = 4;
+            grid[1, 1] = 5;
+            grid[1, 2] = 6;
+            grid[2, 0] = 7;
+            grid[2, 1] = 8;
+            grid[2, 2] = 9;
+     
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(grid[i, j]);
+                }
+                Console.WriteLine();
+               
+            }
+            Console.ReadLine();
         }
     }
 }
