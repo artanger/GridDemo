39021250c94b3cd2457a639b5fc34aa63203e036
Sergey Ivanov <sergey@rainjocks.com>
2020-02-16 21:33:32 +0200

    Update Program.cs
    new iteraction

--- Program.cs
+++ Program.cs
@@ -1,59 +1,182 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GridDemo
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            //int[,] grid = new int[3, 3];
-            //grid[0, 0] = 1;
-            //grid[0, 1] = 2;
-            //grid[0, 2] = 3;
-            //grid[1, 0] = 4;
-            //grid[1, 1] = 5;
-            //grid[1, 2] = 6;
-            //grid[2, 0] = 7;
-            //grid[2, 1] = 8;
-            //grid[2, 2] = 9;
+            int cols = 3;
+            int rows = 3;
+            var grid = new int[cols, rows];
 
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    for (int j = 0; j < 3; j++)
-            //    {
-            //        Console.Write(grid[i, j]);
-            //    }
-            //    Console.WriteLine();
+            //FillInGridCustom(cols, rows, grid);
 
-            //}
-            //Console.ReadLine();
+            ShowDefaultGrid(cols, rows, grid);
 
+            var selected = FillInGridCheckNeighbors(cols, rows, grid);
 
-            int[,] grid = new int[3, 3];
-            int r, c;
-            for (r = 0; r<3; r++)
+            var colNumber = selected[0];
+            var rowNumber = selected[1];
+
+            if (colNumber > -1 || rowNumber > -1) // check that we have entered values without error
             {
-                for (c = 0; c < 3; c++)
+                CheckNeighbors(cols, rows, grid, colNumber, rowNumber);
+
+                ShowGridNeighbors(cols, rows, grid);
+            }
+            
+            Console.ReadLine();
+        }
+
+        private static void FillInGridCustom(int cols, int rows, int[,] grid)
+        {
+            int r, c;
+            for (r = 0; r < cols; r++)
+            {
+                for (c = 0; c < rows; c++)
                 {
-                    Console.Write("row&column - {0},{1}:", r, c);
-                    grid[r,c] = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("row&column - {1},{0}:", r, c);
+                    grid[r, c] = Convert.ToInt32(Console.ReadLine());
                 }
             }
+
             Console.WriteLine();
             Console.WriteLine("GRID");
 
-            for (r = 0; r < 3; r++)
+            for (r = 0; r < cols; r++)
             {
                 Console.WriteLine();
-                for (c = 0; c < 3; c++)
-                    Console.Write("{0}\t",grid[r, c]);
+                for (c = 0; c < rows; c++)
+                    Console.Write("{0}\t", grid[r, c]);
+            }
+        }
+
+        private static void ShowDefaultGrid(int cols, int rows, int[,] grid)
+        {
+            for (var i = 0; i < cols; i++)
+            {
+                for (var j = 0; j < rows; j++)
+                {
+                    grid[i, j] = 0;
+                }
             }
 
-            Console.ReadLine();
+            Console.WriteLine("=================");
+            Console.WriteLine("--- Empty Grid ---");
+            Console.WriteLine("=================");
+            ShowGrid(cols, rows, grid);
+            Console.WriteLine();
+            Console.WriteLine("=================");
+            Console.WriteLine();
+        }
+
+        private static int[] FillInGridCheckNeighbors(int cols, int rows, int[,] grid)
+        {
+            var result = new int[] {-1, -1};
+
+            Console.Write("Enter the Column Number: ");
+
+            if (!int.TryParse(Console.ReadLine(), out var columnNumber))
+            {
+                Console.Write("Entered Column Number value is different from number.");
+                ShowExitMessage();
+                return result;
+            }
+
+            if (columnNumber >= cols)
+            {
+                Console.Write("Entered Column Number value is greater then array's size.");
+                ShowExitMessage();
+                return result;
+            }
+
+            Console.Write("Enter the Row Number: ");
+
+            if (!int.TryParse(Console.ReadLine(), out var rowNumber))
+            {
+                Console.Write("Entered Row Number value is different from number.");
+                ShowExitMessage();
+                return result;
+            }
+
+            if (rowNumber >= rows)
+            {
+                Console.Write("Entered Row Number value is greater then array's size.");
+                ShowExitMessage();
+                return result;
+            }
+
+            grid[columnNumber, rowNumber] = 1; // set selected
+
+            result = new int[] { columnNumber, rowNumber };
+
+            return result;
+        }
+
+        private static void ShowGrid(int cols, int rows, int[,] grid)
+        {
+            for (var c = 0; c < cols; c++)
+            {
+                Console.WriteLine();
+                for (var r = 0; r < rows; r++)
+                {
+                    Console.Write("{0}\t", grid[r, c]);
+                }
+            }
+        }
+
+        private static void ShowGridNeighbors(int cols, int rows, int[,] grid)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("=================");
+            Console.WriteLine("--- Grid with neighbors ---");
+            Console.WriteLine("=================");
+            ShowGrid(cols, rows, grid);
+            Console.WriteLine();
+            Console.WriteLine("=================");
+            Console.WriteLine();
+        }
+
+        private static void CheckNeighbors(int cols, int rows, int[,] grid, int columnNumber, int rowNumber)
+        {
+            if (columnNumber == 0 && rowNumber == 0) // is first column and is first row
+            {
+                grid[columnNumber + 1, rowNumber] = 2;
+                grid[columnNumber, rowNumber + 1] = 2;
+                grid[columnNumber + 1, rowNumber + 1] = 2;
+            }
+            else if (columnNumber == cols - 1 && rowNumber == 0) // is last column and is first row
+            {
+                grid[columnNumber - 1, rowNumber] = 2;
+                grid[columnNumber - 1, rowNumber + 1] = 2;
+                grid[columnNumber, rowNumber + 1] = 2;
+            }
+            else if (rowNumber == 0) // is not last and is not first column and is first row
+            {
+                grid[columnNumber - 1, rowNumber] = 2;
+                grid[columnNumber - 1, rowNumber + 1] = 2;
+                grid[columnNumber, rowNumber + 1] = 2;
+                grid[columnNumber + 1, rowNumber] = 2;
+                grid[columnNumber + 1, rowNumber + 1] = 2;
+            }
+            else if (columnNumber == 0 && rowNumber == rows - 1) // is first column and is last row
+            {
+                grid[columnNumber, rowNumber - 1] = 2;
+                grid[columnNumber + 1, rowNumber - 1] = 2;
+                grid[columnNumber + 1, rowNumber] = 2;
+            }
+
+        }
+
+        private static void ShowExitMessage()
+        {
+            Console.Write("Please, rerun the application entering correct value.");
         }
     }
 }
